a3c1a33f1e71d0aaaa6c2699522b4413a6c1eba0
Amber Cronin <amber@amber.vision>
2024-04-22 13:19:55 +0100

    FPO-153: Handle token expiry in controller

--- src/controllers/authTokenController.ts
+++ src/controllers/authTokenController.ts
@@ -11,35 +11,49 @@ export class AuthTokenController {
   readonly #authUrl: string
   readonly #clientId: string
   readonly #clientSecret: string
+  token: string
+  #tokenExpiresTimestamp: number = 0
 
   constructor (authUrl: string, clientId: string, clientSecret: string) {
     this.#authUrl = authUrl
     this.#clientId = clientId
     this.#clientSecret = clientSecret
+    this.token = ''
   }
 
-  public async fetchToken (): Promise<ClientCredentialsResponse> {
+  public async fetchToken (): Promise<string> {
     return await new Promise((resolve, reject) => {
-      const request = new Request(
-        this.#authUrl,
-        {
-          method: 'POST',
-          headers: {
-            'content-type': 'application/x-www-form-urlencoded'
-          },
-          body: new URLSearchParams({
-            client_id: this.#clientId,
-            client_secret: this.#clientSecret,
-            grant_type: 'client_credentials',
-            scope: 'hub-backend-server/api'
-          })
-        }
-      )
+      const now = Math.floor(Date.now() / 1000)
+      const tokenExpired = now >= this.#tokenExpiresTimestamp
 
-      fetch(request)
-        .then(async (response) => await response.json())
-        .then((data: ClientCredentialsResponse) => { resolve(data) })
-        .catch((err) => { reject(err) })
+      if (this.#tokenExpiresTimestamp === 0 || tokenExpired) {
+        const request = new Request(
+          this.#authUrl,
+          {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+              client_id: this.#clientId,
+              client_secret: this.#clientSecret,
+              grant_type: 'client_credentials',
+              scope: 'hub-backend-server/api'
+            })
+          }
+        )
+
+        fetch(request)
+          .then(async (response) => await response.json())
+          .then((data: ClientCredentialsResponse) => {
+            this.#tokenExpiresTimestamp = now + data.expires_in
+            this.token = data.access_token
+            resolve(this.token)
+          })
+          .catch((err) => { reject(err) })
+      } else {
+        resolve(this.token)
+      }
     })
   }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import express, { type Request, type Response, type NextFunction } from 'express'
 import { HealthchecksController } from '../controllers/healthchecksController'
-import { AuthTokenController } from '../controllers/authTokenController'
 
 const authUrl = process.env.AUTH_URL ?? undefined
 const clientId = process.env.CLIENT_ID ?? undefined
@@ -11,12 +10,7 @@ if (clientId === undefined) throw new Error('CLIENT_ID undefined.')
 if (clientSecret === undefined) throw new Error('CLIENT_SECRET undefined.')
 
 const healthchecksController = new HealthchecksController()
-const authTokenController = new AuthTokenController(authUrl, clientId, clientSecret)
 const router = express.Router()
-
-router.get('/test', async (req, res) => {
-  const token = await authTokenController.fetchToken()
-})
 
 router.get('/healthcheck', (req, res) => { healthchecksController.show(req, res) })
 router.get('/healthcheckz', (req, res) => { healthchecksController.showz(req, res) })
