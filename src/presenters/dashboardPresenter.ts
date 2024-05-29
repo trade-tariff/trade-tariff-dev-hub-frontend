a991d19b0828b5e77ee688fce3f28f4206477e90
William Fish <william.michael.fish@gmail.com>
2024-05-29 17:02:26 +0100

    FPO-242: Incorporate feedback on the dashboard page

--- src/presenters/dashboardPresenter.ts
+++ src/presenters/dashboardPresenter.ts
@@ -12,11 +12,11 @@ interface DashboardTable {
 export namespace DashboardPresenter {
   export function present (apiKeys: ApiKey[], organisationId: string): DashboardTable {
     const headers = [
-      { text: 'API Client ID' },
+      { text: 'API Key' },
       { text: 'Description' },
       { text: 'Created on' },
       { text: 'Status' },
-      { text: 'Revoke' }
+      { text: '' }
     ]
     const rows = apiKeys.map(key => {
       const shouldDelete = !key.Enabled && deletionEnabled
@@ -67,6 +67,9 @@ export namespace DashboardPresenter {
 
   function formatDate (dateInput: Date | string): string {
     const date = new Date(dateInput)
+
+    if (isToday(date)) return 'Today'
+
     if (isNaN(date.getTime())) {
       throw new Error('Invalid date')
     }
@@ -75,5 +78,12 @@ export namespace DashboardPresenter {
       month: 'long',
       year: 'numeric'
     })
+  }
+
+  function isToday (date: Date): boolean {
+    const today = new Date()
+    return date.getDate() === today.getDate() &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear()
   }
 }
