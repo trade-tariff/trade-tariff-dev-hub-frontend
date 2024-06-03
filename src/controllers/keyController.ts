3a7b812ae22c699cb7ec4bd7f76b6bef526fbdaa
Moussa Kayad <mkayad@gmail.com>
2024-06-03 15:17:01 +0100

    FPO-245 Review content and add validation error to new key form

--- src/controllers/keyController.ts
+++ src/controllers/keyController.ts
@@ -2,6 +2,7 @@ import { type Request, type Response } from 'express'
 import { ApiService } from '../services/apiService'
 import { ApiKeyPresenter } from '../presenters/apiKeyPresenter'
 import { logger } from '../config/logging'
+import { validationResult } from 'express-validator'
 
 export const newKey = async (_req: Request, res: Response): Promise<void> => {
   try {
@@ -14,15 +15,17 @@ export const newKey = async (_req: Request, res: Response): Promise<void> => {
 
 export const create = async (req: Request, res: Response): Promise<void> => {
   const apiKeyDescription = req.body.apiKeyDescription as string
+  // Extract the validation errors from a request.
+  const result = validationResult(req)
 
   try {
-    if (apiKeyDescription === '') {
-      res.render('newKey')
-    } else {
+    if (result.isEmpty()) {
       const user = await ApiService.handleRequest(req)
       const apiKey = await ApiService.createAPIKey(user, apiKeyDescription)
       const secretHtml = ApiKeyPresenter.secretHtml(apiKey.Secret)
       res.render('keySuccessPage', { secretHtml })
+    } else {
+      res.render('newKey', { error: result })
     }
   } catch (error) {
     logger.error('Error showing create page', error)
