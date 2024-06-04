01ad0505f396deae0781c805c26147aed15ea406
Moussa Kayad <mkayad@gmail.com>
2024-06-04 16:52:17 +0100

    FPO-238: Cookies and Privacy Policy placeholder pages

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@
 import express, { type Router, type Request, type Response, type NextFunction } from 'express'
 import { HealthchecksController } from '../controllers/healthchecksController'
 import { newVerificationPage, checkVerificationDetails, applicationComplete } from '../controllers/verificationController'
+import { privacyPolicyPage } from '../controllers/privacyPolicyController'
+import { cookiesPage } from '../controllers/cookiesController'
 
 const healthchecksController = new HealthchecksController()
 const router: Router = express.Router()
@@ -10,6 +12,8 @@ const router: Router = express.Router()
 router.get('/', (_req: Request, res: Response, _next: NextFunction) => { res.render('index.njk') })
 router.get('/healthcheck', (req, res) => { healthchecksController.show(req, res) })
 router.get('/healthcheckz', (req, res) => { healthchecksController.showz(req, res) })
+router.get('/privacyPolicy', privacyPolicyPage)
+router.get('/cookies', cookiesPage)
 
 router.get('/:id/verification', newVerificationPage)
 router.post('/:id/check-verification', checkVerificationDetails)
