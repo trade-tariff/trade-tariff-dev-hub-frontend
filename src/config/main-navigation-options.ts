20087c2ce830ea2601815076afb8c4c51712249a
William Fish <william.michael.fish@gmail.com>
2024-06-11 09:46:21 +0100

    FPO-271: Code review

--- src/config/main-navigation-options.ts
+++ src/config/main-navigation-options.ts
@@ -2,22 +2,24 @@ import { type NextFunction, type Response } from 'express'
 
 export default function (req: any, res: Response, next: NextFunction): void {
   const mainNavigation: any = []
+  const isAuthenticated: boolean = req.oidc?.isAuthenticated() ?? false
 
-  if (req.oidc?.isAuthenticated()) {
+  if (isAuthenticated) {
     mainNavigation.push({
       href: '/dashboard',
       text: 'Dashboard'
     })
 
-    const userProfile = req.appSession?.userProfile ?? null
-    console.log('UserProfileFound:', userProfile)
+    const userProfile = req.appSession?.userProfile ?? {}
+    const profileUrl = userProfile.profile ?? null
+    const groupUrl = userProfile['bas:groupProfile'] ?? null
 
     mainNavigation.push({
-      href: '/userProfile',
+      href: profileUrl,
       text: 'Update Profile'
     })
     mainNavigation.push({
-      href: '/team',
+      href: groupUrl,
       text: 'Manage Team'
     })
     mainNavigation.push({
