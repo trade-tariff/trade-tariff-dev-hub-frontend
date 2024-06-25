a7f0016d9b1b06b015a1ca331a59234b7834db69
Moussa Kayad <mkayad@gmail.com>
2024-06-25 12:21:54 +0100

    FPO-274 Check Organisation status after creating the user

--- src/config/registration.ts
+++ src/config/registration.ts
@@ -6,10 +6,15 @@ import { logger } from '../config/logging'
 export default async function (req: Request, res: Response, next: NextFunction): Promise<void> {
   const user = CommonService.handleRequest(req)
   const scpUser = { userId: user.userId, groupId: user.groupId }
-  const userInfo = await UserService.getUser(scpUser)
+  let userInfo = await UserService.getUser(scpUser)
 
   logger.debug(`User info: ${JSON.stringify(userInfo)}`)
   logger.debug(`User info status: ${userInfo.Status}`)
+  if (userInfo.Status === undefined) {
+    await UserService.createUser(scpUser)
+  }
+  userInfo = await UserService.getUser(scpUser)
+  logger.debug(`After create: User info status: ${userInfo.Status}`)
   try {
     switch (userInfo.Status) {
       case 'Authorised':
@@ -28,10 +33,10 @@ export default async function (req: Request, res: Response, next: NextFunction):
         logger.debug('User is unregistered')
         res.render('verification')
         break
-      default:
-        logger.debug(`User status not recognised ${userInfo.Status}`)
-        await UserService.createUser(scpUser)
-        res.render('verification')
+      default: {
+        logger.debug(`Default case: User status ${userInfo.Status}`)
+        next()
+      }
     }
   } catch (error) {
     logger.error('Error in the registration flow:', error)
