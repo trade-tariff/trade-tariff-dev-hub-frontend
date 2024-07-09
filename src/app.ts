dda938f1c46d67d3c02df73b962c18e24485075b
Ashley Ward <421038+ashward@users.noreply.github.com>
2024-07-09 22:11:34 +0100

    Linting

--- src/app.ts
+++ src/app.ts
@@ -76,7 +76,7 @@ app.use(express.static('public'))
 app.use(cookieSession({
   name: 'session',
   keys: [cookieSigningSecret],
-  maxAge: 24 * 60 * 60 * 1000,
+  maxAge: 24 * 60 * 60 * 1000
 }))
 
 app.use(csrf())
