e90a32a2141e92836e6a3176b1b1ce02c6775813
Ashley Ward <421038+ashward@users.noreply.github.com>
2024-07-10 15:48:56 +0100

    Removing prettier and switching to the old linting for the time being

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,43 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintConfigPrettier,
-];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/dist/", "**/node_modules/"],
+}, ...compat.extends("standard-with-typescript"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"])),
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 2021,
+        sourceType: "commonjs",
+
+        parserOptions: {
+            project: "./tsconfig.json",
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/unbound-method": "off",
+        "@typescript-eslint/no-namespace": "off",
+    },
+}];
