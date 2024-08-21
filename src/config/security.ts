ce95d90b95a583a0cccbb10458a210cde3500800
Ashley Ward <421038+ashward@users.noreply.github.com>
2024-08-21 12:00:52 +0100

    Make the cspNonce call non-blocking

--- src/config/security.ts
+++ src/config/security.ts
@@ -4,8 +4,14 @@ import helmet from 'helmet'
 
 export const configureSecurity = (app: Application): void => {
   app.use((req, res, next) => {
-    res.locals.cspNonce = crypto.randomBytes(32).toString('hex')
-    next()
+    crypto.randomBytes(32, (err, buffer) => {
+      if (err !== null) {
+        next(err); return
+      }
+
+      res.locals.cspNonce = buffer.toString('hex')
+      next()
+    })
   })
   app.use(
     helmet({
